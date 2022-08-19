f1ed56f86a59253e8f48c0475033056587ae734d
Cemdeniz Cay <cemdenizcay@gmail.com>
2022-08-19 13:54:46 +0300

    First commit

---
+++ reddit/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from reddit.users.models import User
+from reddit.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
